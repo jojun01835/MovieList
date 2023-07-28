d1c1feff9724fdb63614c40249ed419c33f00d5f
jojun0 <jojun01835@gmail.com>
2023-07-28 18:08:37 +0900

    게시판 구현 완료

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,12 +1,25 @@
+// PostForm.js
 import React, { useState } from "react";
 import { collection, addDoc } from "firebase/firestore";
 import { firestore } from "../firebase";
+import Rating from "react-rating-stars-component";
+import "./PostForm.css"; // PostForm 컴포넌트의 스타일 파일 임포트
 
 const PostForm = () => {
   const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [rating, setRating] = useState(0);
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
+  };
+
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleRatingChange = (newRating) => {
+    setRating(newRating);
   };
 
   const handleSubmit = async (e) => {
@@ -15,22 +28,44 @@ const PostForm = () => {
     try {
       await addDoc(collection(firestore, "User"), {
         title: title,
+        content: content,
+        rating: rating, // 별점도 함께 저장
+        timestamp: new Date(),
       });
 
       setTitle("");
+      setContent("");
+      setRating(0);
     } catch (error) {
       console.log("Error adding document:", error);
     }
   };
 
   return (
-    <div>
+    <div className="post-form">
       <h2>Add New Post</h2>
       <form onSubmit={handleSubmit}>
         <label>
           Title:
           <input type="text" value={title} onChange={handleTitleChange} />
         </label>
+        <br />
+        <label>
+          Content:
+          <textarea value={content} onChange={handleContentChange} />
+        </label>
+        <br />
+        <label>
+          Rating:
+          <Rating
+            count={5}
+            size={24}
+            activeColor="#ffd700"
+            value={rating}
+            onChange={handleRatingChange} // 별점 입력을 처리하는 함수
+          />
+        </label>
+        <br />
         <button type="submit">Add Post</button>
       </form>
     </div>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { firestore, collection, getDocs, deleteDoc, doc, onSnapshot } from "firebase/firestore"; // deleteDoc 메서드 import 추가
+import { firestore, collection, getDocs, deleteDoc, doc, onSnapshot, updateDoc } from "firebase/firestore";
 import { firestore as db } from "../firebase";
+import Rating from "react-rating-stars-component";
+import "./PostList.css"; // CSS 파일 경로를 적절하게 수정해주세요
+
+const formatDate = (timestamp) => {
+  if (!timestamp) return "";
+
+  const date = timestamp.toDate();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minute = String(date.getMinutes()).padStart(2, "0");
+
+  return `${year}-${month}-${day} ${hour}:${minute}`;
+};
 
 const PostList = () => {
   const [posts, setPosts] = useState([]);
+  const [editingPostId, setEditingPostId] = useState(null);
+  const [rating, setRating] = useState(0); // 추가: 별점 상태
 
   useEffect(() => {
-    // 실시간으로 데이터 감시하기
+    // Firestore의 데이터를 실시간으로 감시하여 변경되면 화면을 다시 렌더링
     const unsubscribe = onSnapshot(collection(db, "User"), (querySnapshot) => {
       const postData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
       setPosts(postData);
@@ -14,8 +31,34 @@ const PostList = () => {
 
     // 컴포넌트가 언마운트될 때 감시 중단
     return () => unsubscribe();
-  }, []);
+  }, []); // 빈 배열을 두 번째 인자로 전달하여 한 번만 실행하도록 설정
 
+  // 수정 중인 게시물 ID 설정 함수
+  const handleEdit = (postId, title, content, rating) => {
+    setEditingPostId(postId);
+    setRating(rating); // 수정 중인 게시물의 별점을 설정
+  };
+
+  // 게시물 업데이트 함수
+  const handleUpdate = async (postId, title, content) => {
+    try {
+      // 게시물 업데이트
+      await updateDoc(doc(db, "User", postId), {
+        title: title,
+        content: content,
+        timestamp: new Date(),
+        isEdited: true,
+        rating: rating, // 업데이트 시 기존 별점을 유지
+      });
+
+      setEditingPostId(null); // 수정 완료 후 수정 중인 게시물 ID 초기화
+      setRating(0); // 별점 초기화
+    } catch (error) {
+      console.log("Error updating document:", error);
+    }
+  };
+
+  // 게시물 삭제 함수
   const handleDelete = async (postId) => {
     try {
       await deleteDoc(doc(db, "User", postId));
@@ -25,12 +68,46 @@ const PostList = () => {
   };
 
   return (
-    <div>
+    <div className="post-list">
       <h2>Post List</h2>
       <ul>
         {posts.map((post) => (
-          <li key={post.id}>
-            {post.title} <button onClick={() => handleDelete(post.id)}>Delete</button>
+          <li className="post-item" key={post.id}>
+            {editingPostId === post.id ? (
+              <div>
+                <input type="text" value={post.title} onChange={(e) => setPosts(posts.map((p) => (p.id === post.id ? { ...p, title: e.target.value } : p)))} />
+                <textarea value={post.content} onChange={(e) => setPosts(posts.map((p) => (p.id === post.id ? { ...p, content: e.target.value } : p)))} />
+                <Rating
+                  count={5}
+                  size={24}
+                  activeColor="#ffd700"
+                  value={rating} // 수정 중인 게시물의 별점을 표시
+                  onChange={(newRating) => setRating(newRating)} // 별점이 변경되면 상태 업데이트
+                />
+                <button className="update-btn" onClick={() => handleUpdate(post.id, post.title, post.content)}>
+                  수정 완료
+                </button>
+                <button className="cancel-btn" onClick={() => setEditingPostId(null)}>
+                  취소
+                </button>
+              </div>
+            ) : (
+              <>
+                <div className="post-info">
+                  <strong>{post.title}</strong> ({formatDate(post.timestamp)}){post.isEdited && <span className="edited"> - 수정됨</span>}
+                </div>
+                <Rating count={5} size={24} activeColor="#ffd700" value={post.rating} edit={false} />
+                <div>
+                  <button className="edit-btn" onClick={() => handleEdit(post.id, post.title, post.content, post.rating)}>
+                    Edit
+                  </button>
+                  <button className="delete-btn" onClick={() => handleDelete(post.id)}>
+                    Delete
+                  </button>
+                </div>
+                <p>{post.content}</p>
+              </>
+            )}
           </li>
         ))}
       </ul>
